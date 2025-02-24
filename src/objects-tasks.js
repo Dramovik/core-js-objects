eab1da1f1b8d4ac2698effef4bf9ecc3ad77d879
Dramovik <dramovik1@gmail.com>
2025-02-24 22:28:52 +0200

    feat: complete removeProperties

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -59,8 +59,13 @@ function mergeObjects(objects) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, ['age']) => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+function removeProperties(obj, keys) {
+  const resultObj = {};
+  Object.assign(resultObj, obj);
+  keys.forEach((item) => {
+    delete resultObj[item];
+  });
+  return resultObj;
 }
 
 /**
