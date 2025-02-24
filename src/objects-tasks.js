bb3f22222b17d14e839f843339e2fea4771b8231
Dramovik <dramovik1@gmail.com>
2025-02-24 23:49:26 +0200

    feat: complete makeWord

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -138,8 +138,20 @@ function makeImmutable(obj) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  const revertObj = {};
+  let result = '';
+  let length = 0;
+  Object.entries(lettersObject).forEach(([key, val]) => {
+    val.forEach((item) => {
+      revertObj[item] = key;
+    });
+  });
+  length = Object.keys(revertObj).length;
+  for (let i = 0; i < length; i += 1) {
+    result += revertObj[i];
+  }
+  return result;
 }
 
 /**
