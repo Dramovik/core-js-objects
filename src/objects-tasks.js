39d3558b8bb0c09a20af04b0e8694b811097d868
Dramovik <dramovik1@gmail.com>
2025-02-25 04:22:31 +0200

    feat: complete fromJSON

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -249,8 +249,10 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const newJson = JSON.parse(json);
+  const newObj = Object.create(proto);
+  return Object.assign(newObj, newJson);
 }
 
 /**
